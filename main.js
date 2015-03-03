1b2852f528122c619815253c59a19beb29f60820
emilemuny <emilemuny007@gmail.com>
2015-03-02 17:32:29 -0800

    adding code progress/added list and main templates

---
+++ main.js
@@ -0,0 +1,12 @@
+//our two controllers, listOfTodos and eachTodo.
+
+angular.module("todo")
+.controller("listOfTodos", ["$scope", "$location", "$routeParams", "ListService",
+  function($scope, $location, $routeParams, ListService) {
+ }])
+
+.controller("eachTodo", ["$scope", "$location", "$routeParams", "ListService",
+  function($scope, $location, $routeParams, ListService) {
+
+ }]);
+
